fb7c12379a5f9498f4bc28af3e6fba1388c96ca5
saifulislam05 <saiful.bijulia@gmail.com>
2023-11-10 18:21:50 +0530

    added feature highlighting matched text with adding text color

--- script.js
+++ script.js
@@ -10,16 +10,36 @@ function renderUi(list) {
   const fragment = document.createDocumentFragment();
 
   list.forEach((item) => {
-
     let row = document.createElement("tr");
+    // Highlight the matched text in the capital and country columns
+    const capitalHtml = highlightMatchedText(item.capital, inputBox.value);
+    const countryHtml = highlightMatchedText(item.country, inputBox.value);
     row.innerHTML = `
-              <td class="text-base">${item.capital}</td>
-              <td class="text-base">${item.country}</td>
+              <td class="text-base">${capitalHtml}</td>
+              <td class="text-base">${countryHtml}</td>
             `;
     fragment.appendChild(row);
   });
 
   tableBody.appendChild(fragment);
+}
+
+function highlightMatchedText(text, searchTerm) {
+  // If there's no search term, return the original text
+  if (!searchTerm) {
+    return text;
+  }
+
+  // Create a RegExp to match the search term case-insensitively
+  const regex = new RegExp(searchTerm, "ig");
+
+  // Replace the matched text with a span having a specific class for styling
+  const highlightedText = text.replace(
+    regex,
+    (match) => `<span  style="color:#e9ff32">${match}</span>`
+  );
+
+  return highlightedText;
 }
 
 inputBox.addEventListener("input", filterByInput);
