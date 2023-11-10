9c818bb934ae525ed97b583dad4a65cf17ae94d5
saifulislam05 <saiful.bijulia@gmail.com>
2023-11-10 17:53:06 +0530

    design done

---
+++ countries.js
@@ -0,0 +1,83 @@
+
+  const countries= [
+    {
+      "country": "United States",
+      "capital": "Washington, D.C."
+    },
+    {
+      "country": "United Kingdom",
+      "capital": "London"
+    },
+    {
+      "country": "Canada",
+      "capital": "Ottawa"
+    },
+    {
+      "country": "France",
+      "capital": "Paris"
+    },
+    {
+      "country": "Germany",
+      "capital": "Berlin"
+    },
+    {
+      "country": "Japan",
+      "capital": "Tokyo"
+    },
+    {
+      "country": "India",
+      "capital": "New Delhi"
+    },
+    {
+      "country": "Australia",
+      "capital": "Canberra"
+    },
+    {
+      "country": "Brazil",
+      "capital": "Brasília"
+    },
+    {
+      "country": "South Africa",
+      "capital": "Pretoria"
+    },
+    {
+      "country": "China",
+      "capital": "Beijing"
+    },
+    {
+      "country": "Russia",
+      "capital": "Moscow"
+    },
+    {
+      "country": "Italy",
+      "capital": "Rome"
+    },
+    {
+      "country": "Spain",
+      "capital": "Madrid"
+    },
+    {
+      "country": "Mexico",
+      "capital": "Mexico City"
+    },
+    {
+      "country": "Argentina",
+      "capital": "Buenos Aires"
+    },
+    {
+      "country": "Egypt",
+      "capital": "Cairo"
+    },
+    {
+      "country": "South Korea",
+      "capital": "Seoul"
+    },
+    {
+      "country": "Nigeria",
+      "capital": "Abuja"
+    },
+    {
+      "country": "Saudi Arabia",
+      "capital": "Riyadh"
+    }
+  ]
